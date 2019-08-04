c3a1579afc06eeade7d8b916b1bae5e2b3719df2
unknown <grattoni.fabio@gmail.com>
2019-08-04 19:32:25 +0200

    Update: DateInput function. Also fix bug in _main_

--- main.py
+++ main.py
@@ -1,67 +1,62 @@
 from datetime import datetime
 from datetime import timedelta
 
-def DateInput(error = 1):
+
+def DateInput():
     """Function to verify that the date inserted is a valid one:
         i.e. in the right form, in numbers, in the future and an existing day 
     """
-    """variable 'error' represents if the date is correct (= 0) or not (= 1)
-        - error starts = 0
-        - error becomes = 1 if an error is spotted
-        - while loop runs as long as errors in the input are found.
-    """
-    while(error == 1):
-        error = 0
+    while(True):
             
         # check if input is in the right form
         try:
             day, month, year = input("Please insert the date in the form of dd-mm-yyyy: ").split("-")
         except:
-            error = 1
             print("You inserted a date in an invalid format.")
+            continue
         
         # check if only numbers are provided
-        if(error == 0):
-            if( day.isdigit() and month.isdigit() and year.isdigit() ):
-                day = int(day)
-                month = int(month)
-                year = int(year)
-            else:
-                error = 1
-                print("You should only insert numbers.")
+        if( day.isdigit() and month.isdigit() and year.isdigit() ):
+            day = int(day)
+            month = int(month)
+            year = int(year)
+        else:
+            print("You should only insert numbers.")
+            continue
 
         # check if the input is a valid date
-        if(error == 0):
-            try:
-                date = datetime(day = day, month = month, year = year)
-            except:
-                error = 1
-                print(f"The date {day}/{month}/{year} is not valid.")
+        try:
+            date = datetime(day = day, month = month, year = year)
+        except:
+            print(f"The date {day}/{month}/{year} is not valid.")
+            continue
 
         # check if the date is in the past
-        if(error == 0):
-            if ( not (date < datetime.now()) ):
-                error = 1
-                print("The date inserted should be in the past")
+        if ( not (date < datetime.now()) ):
+            print("The date inserted should be in the past")
+            continue
+        else:
+            break
 
     return date
 
+
 if __name__ == "__main__":
-    print("WELCOME IN THE PROGRAM.")
+    print("\nWELCOME IN THE PROGRAM.")
     today = datetime.now()
     print(f"Today is the {today.day}/{today.month}/{today.year}\n")
 
     date = DateInput()
-    print(f"The date inserted is {date[0]}/{date[1]}/{date[2]}.\n")
+    print(f"The date inserted is {date.day}/{date.month}/{date.year}.\n")
 
     print("Would you like the program to consider also the time of the event?") 
-    print(f"If no then the program will consider {date[0]}/{date[1]}/{date[2]} 12:00am as the starting date.")
+    print(f"If no then the program will consider {date.day}/{date.month}/{date.year} 12:00am as the starting date.")
     timeAnswer = input("Answer by y/n: ")
 
     while(timeAnswer not in ["y", "n", "Y", "N"]):
         timeAnswer = input("Please insert a valid answer y/n: ")
 
-    if(timeAnswer==y):
+    if(timeAnswer=="y"):
         pass
 
     date = date.replace(hour=hour, minute=minute)
